763f72d11db6858242ba34b43adb33ac10401fa2
Levi Aviv <leviaviv28@gmail.com>
2019-06-16 17:09:36 -0400

    Rewrote code to be compatible with my cartidge based console project
    Added new features such as:
        GameOver and MainMenu screen
        Persistent highscore
        +Much more

--- Flappy.h
+++ Flappy.h
@@ -1,32 +1,14 @@
 #ifndef _Flappy_H
 #define _Flappy_H
 
-// Colours
-#define BLACK   0x0000
-#define DGREEN  0x0400
-#define GREEN   0x07E0
-#define CYAN    0x07FF
-#define YELLOW  0xFFE0
-#define WHITE   0xFFFF
-#define LYELLOW 0xFFF8
-#define LGREY   0xCE59
-#define RED     0xF208
-#define ORANGE  0xFBA0
-//Pins
-#define YP A2
-#define XM A3
-#define YM 8
-#define XP 9
-//Touchscreen Values
-#define TS_MINX 130
-#define TS_MAXX 905
-#define TS_MINY 75
-#define TS_MAXY 930
-#define MINPRESSURE 10
-#define MAXPRESSURE 1000
-#define TICKS_PER_SECOND 1
-#define SKIP_TICKS 1 / TICKS_PER_SECOND; // FPS
+#include "ConsoleInterface.h"
+#include <EEPROM.h>
+
+#define TICKS_PER_SECOND 60
+#define SKIP_TICKS 1800 / TICKS_PER_SECOND; // FPS
 #define MAX_FRAMESKIP 1
+#define WIDTH 480
+#define HEIGHT 320
 
 typedef struct {
   int x;
--- Flappy.ino
+++ Flappy.ino
@@ -1,125 +1,98 @@
-#include <Adafruit_GFX.h>
-#include <Adafruit_TFTLCD.h>
-#include <TouchScreen.h>
-#include <MCUFRIEND_kbv.h>
 #include "Flappy.h"
 
-
+ConsoleInterface console;
 unsigned long next_game_tick;
 int loops;
-
-
-MCUFRIEND_kbv tft;
-TouchScreen ts = TouchScreen(XP, YP, XM, YM, 300);
-TSPoint p;
 
 bool tapped = false;
 bool jump = false;
 bool scored = false;
+int cur_highScore = EEPROM.read(0);
 
-Player player = {75, tft.width()/2, 17, 12, 3, 0, true};
-Pipe pipes = Pipe{480, 100, 26, 7, 125};
+Player player = {75, HEIGHT/2, 25, 25, 0, 0, false};
+Pipe pipes = Pipe{480, 100, 26, 15, 125};
 
-uint16_t player_sprite[12][17] = { {CYAN,CYAN,CYAN,CYAN,CYAN,CYAN,BLACK,BLACK,BLACK,BLACK,BLACK,BLACK,CYAN,CYAN,CYAN,CYAN,CYAN},
-                                   {CYAN,CYAN,CYAN,CYAN,BLACK,BLACK,LYELLOW,LYELLOW,LYELLOW,BLACK,WHITE,WHITE,BLACK,CYAN,CYAN,CYAN,CYAN},
-                                   {CYAN,CYAN,CYAN,CYAN,BLACK,LYELLOW,LYELLOW,YELLOW,YELLOW,BLACK,WHITE,WHITE,WHITE,WHITE,BLACK,CYAN,CYAN},
-                                   {CYAN,CYAN,BLACK,LYELLOW,YELLOW,YELLOW,YELLOW,YELLOW,BLACK,LGREY,WHITE,WHITE,BLACK,WHITE,BLACK,CYAN,CYAN},
-                                   {CYAN,BLACK,YELLOW,YELLOW,YELLOW,YELLOW,YELLOW,YELLOW,BLACK,LGREY,WHITE,WHITE,BLACK,WHITE,BLACK,CYAN,CYAN},
-                                   {CYAN,BLACK,BLACK,BLACK,BLACK,BLACK,YELLOW,YELLOW,YELLOW,YELLOW,BLACK,LGREY,WHITE,WHITE,WHITE,BLACK,CYAN},
-                                   {BLACK,LYELLOW,LYELLOW,LYELLOW,LYELLOW,LYELLOW,BLACK,YELLOW,YELLOW,YELLOW,BLACK,BLACK,BLACK,BLACK,BLACK,BLACK,CYAN},
-                                   {BLACK,YELLOW,LYELLOW,LYELLOW,LYELLOW,YELLOW,BLACK,YELLOW,YELLOW,BLACK,RED,RED,RED,RED,RED,RED,BLACK},
-                                   {CYAN,BLACK,BLACK,BLACK,BLACK,BLACK,ORANGE,ORANGE,BLACK,RED,BLACK,BLACK,BLACK,BLACK,BLACK,BLACK,CYAN},
-                                   {CYAN,CYAN,BLACK,ORANGE,ORANGE,ORANGE,ORANGE,ORANGE,ORANGE,BLACK,RED,RED,RED,RED,RED,BLACK,CYAN},
-                                   {CYAN,CYAN,CYAN,BLACK,BLACK,ORANGE,ORANGE,ORANGE,ORANGE,ORANGE,BLACK,BLACK,BLACK,BLACK,BLACK,CYAN,CYAN},
-                                   {CYAN,CYAN,CYAN,CYAN,CYAN,BLACK,BLACK,BLACK,BLACK,BLACK,CYAN,CYAN,CYAN,CYAN,CYAN,CYAN,CYAN}};
+byte flappySprite[25] = {CLEAR,YELLOW,WHITE,YELLOW,CLEAR,
+                         YELLOW,YELLOW,WHITE,BLACK,YELLOW,
+                         YELLOW,YELLOW,YELLOW,YELLOW,YELLOW,
+                         YELLOW,BLACK,BLACK,RED,RED,
+                         CLEAR,YELLOW,YELLOW,YELLOW,CLEAR};
 
 void setup() {
-  Serial.begin(9600);
   randomSeed(analogRead(0));
-  tft.begin(0x9486);
-  tft.setRotation(3);
-  tft.fillScreen(CYAN);
-  tft.setTextSize(5);
+  console.screen.storeSprite(flappySprite, 0);
+  console.screen.fillScreen(CYAN);
+  drawMainMenu();
 }
 
 void loop() {
   gameLoop();
 }
 
+void drawMainMenu(){
+  drawGround();
+  while(console.btnArr[0] != 1){
+    //Waiting to start game
+    console.getInputs();
+    clearPipes();
+    pipePhysics();
+    drawPipes();
+    console.screen.drawText(10, 100, 7, BLACK, "Crappy Bird");
+    console.screen.drawText(140, 190, 2, BLACK, "Press A to start");
+  }
+  resetGame();
+}
+
 void draw(){
-  clear_pipes();
-  draw_ground();
-  draw_pipes();
-  clear_player();
-  draw_player();
-  tft.setCursor(220, 50);
-  tft.print(player.score);
+  drawPipes();
+  //Draw player
+  console.screen.drawSprite(0, player.x, player.y, 5);
+  //Draw score
+  char str[3];
+  itoa(player.score, str, 10);
+  console.screen.drawText(220, 50, 5, 1, str);
  }
 
-void draw_pipes(){
-  //Draw Outlines
-  tft.drawFastVLine(pipes.x, 0, pipes.y, BLACK); //Top left pipe outline
-  tft.drawFastVLine(pipes.x + pipes.width, 0, pipes.y, BLACK); //Top right pipe outline
-  tft.drawFastHLine(pipes.x, pipes.y, pipes.width, BLACK);//Top edge outline
-  tft.drawFastVLine(pipes.x, pipes.y + pipes.gap, 320, BLACK); //Bottom left pipe outline
-  tft.drawFastVLine(pipes.x + pipes.width, pipes.y + pipes.gap, 320, BLACK); //Bottom right pipe outline
-  tft.drawFastHLine(pipes.x, pipes.y + pipes.gap - 1, pipes.width, BLACK);//Bottom edge outline
-  //Fill Pipes
-  for(int i = 1; i < pipes.width; i++){
-    if(i > 4 && i < 9){
-      tft.drawFastVLine(pipes.x + i, 0, pipes.y, GREEN);
-      tft.drawFastVLine(pipes.x + i, pipes.y + pipes.gap, 320, GREEN);
-    }else{
-      tft.drawFastVLine(pipes.x + i, 0, pipes.y, DGREEN);
-      tft.drawFastVLine(pipes.x + i, pipes.y + pipes.gap, 320, DGREEN);
+void drawPipes(){
+  for(int i = 0; i < pipes.speed; i++){
+    console.screen.drawFastVLine(pipes.x - i, 0, pipes.y, DARKGREEN);
+    console.screen.drawFastVLine(pipes.x - i, pipes.y + pipes.gap, HEIGHT - (pipes.y + pipes.gap) - 20, DARKGREEN);
+  }
+}
+
+void clearPipes(){
+  for(int i = pipes.speed; i > 0; i--){
+    console.screen.drawFastVLine(pipes.x + pipes.width + i, 0, pipes.y, CYAN);
+    console.screen.drawFastVLine(pipes.x + pipes.width + i, pipes.y + pipes.gap, HEIGHT - (pipes.y + pipes.gap) - 20, CYAN);
+  }
+}
+
+void clearPlayer(){
+  if(player.y_vel > 0){
+    for(int i = 0; i < 10; i++) {
+      console.screen.drawFastHLine(player.x, player.y - i, player.w, CYAN);
+    }
+  } else if (player.y_vel < 0) {
+    for(int i = 0; i < 10; i++ ){
+      console.screen.drawFastHLine(player.x, player.y + player.h + i, player.w, CYAN);
     }
   }
 }
 
-void clear_pipes(){
-  //Clear Outlines
-  for(int i = 0; i <= pipes.speed; i++){
-    tft.drawFastVLine(pipes.x + pipes.width + i, 0, pipes.y + 1, CYAN); //clears Top pipe outline
-    tft.drawFastVLine(pipes.x + pipes.width + i, pipes.y + pipes.gap - 1, 320, CYAN); //clears bottom right pipe outline
-  }
-}
-
-void draw_player(){
-  for(int y = 0; y < player.h; y++)
-    for(int x = 0; x < player.w; x++){
-      tft.drawPixel(player.x+x, player.y+y, player_sprite[y][x]);
-    }
-}
-void clear_player(){
-  //tft.fillCircle(player.x, player.y - player.y_vel, player.radius, CYAN);
-  if(player.y_vel > 0)
-    for(int y = 0; y < player.y_vel; y++)
-      for(int x = 0; x < player.w; x++)
-        tft.drawPixel(player.x+x, player.y+y - player.y_vel, CYAN);
-  else
-    for(int y = 0; y <= abs(player.y_vel) + 5; y++)
-      for(int x = 0; x < player.w; x++)
-        tft.drawPixel(player.x+x, player.y + player.w + player.y_vel + y, CYAN);
-}
-
-void draw_ground(){
-  int x, y;
-  for(y = 0; y < 10; y++){
-    tft.drawFastHLine(0,tft.height() - y, pipes.x, GREEN);
-    tft.drawFastHLine(pipes.x + pipes.width, tft.height() - y, tft.width() - pipes.x + pipes.width, GREEN);
-  }
+void drawGround(){
+  console.screen.fillRect(0, 300, 480, 20, GREEN);
 }
 
 void pipePhysics(){
   pipes.x -= pipes.speed;
   if(pipes.x + pipes.gap <= 0){
     pipes.x = 480;
-    pipes.y = random(pipes.gap, 320 - pipes.gap);
+    pipes.y = random(pipes.gap, HEIGHT - pipes.gap - 40);
     scored = false;
   }
 }
 
-void player_physics(){
+void playerPhysics(){
   if(collision()){
     player.dead = true;
     player.y_vel = 0;
@@ -144,75 +117,89 @@ void player_physics(){
 bool collision(){
   bool collided = false;
   //Check collision with floor
-  if(player.y + player.h >= tft.height() - 10)
+  if(player.y + player.h >= HEIGHT - 20){
     collided = true;
-  else if(player.x >= pipes.x && player.x <= pipes.x + pipes.width)
-    if(player.y <= pipes.y || player.y - player.h >= pipes.y + pipes.gap)
-      collided = true;
+  }
+  //Check for collision with pipes
+  else if(player.x + player.w > pipes.x && player.x < pipes.x + pipes.width && (player.y < pipes.y || player.y + player.h > pipes.y + pipes.gap)){
+    collided = true;
+  }
   return collided;
 }
 
 void awardScore(){
+  char str[3];
   if(pipes.x < player.x && not scored){
-    tft.setTextColor(CYAN);
-    tft.setCursor(220, 50);
-    tft.print(player.score);
-    tft.setTextColor(WHITE);
-    player.score++;
+    //Clear score text
+    itoa(player.score, str, 10);
+    console.screen.drawText(220, 50, 5, CYAN, str);
+    //Write new score text
+    itoa(++player.score, str, 10);
+    console.screen.drawText(220, 50, 5, BLACK, str);
     scored = true;
   }
 }
 
 void gameLoop(){
+  clearPipes();
+  clearPlayer();
   loops = 0;
   while(!player.dead &&  millis() > next_game_tick && loops < MAX_FRAMESKIP) {
-      player_physics();
       pipePhysics();
+      console.getInputs();
+      if (console.btnArr[0] == 1) {
+        if(!tapped){
+          tapped = true;
+          jump = true;
+        }
+      } else if(tapped) {
+        tapped = false;
+      }
+      playerPhysics();
       awardScore();
       next_game_tick += SKIP_TICKS;
       loops++;
   }
-  draw();
-  digitalWrite(13, HIGH);
-  p = ts.getPoint();
-  digitalWrite(13, LOW);
-  pinMode(XM, OUTPUT);
-  pinMode(YP, OUTPUT);
-  if (p.z > MINPRESSURE && p.z < MAXPRESSURE) {
-    if(not tapped){
-      tapped = true;
-      if(player.dead){
-        player.dead = false;
-        resetGame();
-      }else
-        jump = true;
+  if(player.dead){
+    drawGameOver();
+    console.getInputs();
+    if(console.btnArr[0] == 1){
+      resetGame();
     }
-  } else if(tapped){
-    tapped = false;
+  } else {
+    draw();
   }
+}
+
+void drawGameOver(){
+  char str[3];
+  itoa(player.score, str, 10);
+  console.screen.drawText(220, 50, 5, CYAN, str);
+  console.screen.drawText(60, 50, 7, BLACK, "Game Over");
+  console.screen.drawText(130, 130, 5, BLACK, "Score:");
+  console.screen.drawText(300, 135, 5, BLACK, str);
+  if(player.score >= cur_highScore){
+      EEPROM.write(0, player.score);
+      cur_highScore = player.score;
+      console.screen.drawText(40, 190, 5, RED, "New Highscore!");
+  } else {
+    console.screen.drawText(65, 190, 5, BLACK, "Highscore:");
+    itoa(cur_highScore, str, 10);
+    console.screen.drawText(355, 190, 5, BLACK, str);
+  }
+
 }
 
 void resetGame(){
-  resetGraphics();
-  player.y = tft.height()/2 - player.h/2;
+  player.dead = false;
+  player.y = HEIGHT/2 - player.h/2;
   player.score = 0;
-  pipes.x = tft.width();
+  pipes.x = WIDTH;
+  resetGraphics();
 }
 
 void resetGraphics(){
-  //Clear player from screen
-  for(int x = player.x; x <= player.x + player.w; x++)
-      tft.drawFastVLine(x,player.y, player.h, CYAN);
-  //Clear pipe from screen
-  for(int i = 0; i <= pipes.width; i++){
-      tft.drawFastVLine(pipes.x + i, 0, pipes.y + 1, CYAN);
-      tft.drawFastVLine(pipes.x + i, pipes.y + pipes.gap - 1, 320, CYAN);
-  }
-  //Clear score from screen
-  int score_digits = floor(log10(player.score)) + 1;
-  for(int digit = 0; digit < score_digits; digit++)
-    for(int x = 0; x < 25; x++)
-      tft.drawFastVLine(220 + x + (digit * 30), 50, 35, CYAN);
-
-  
+  console.screen.fillScreen(CYAN);
+  drawGround();
+  draw();  
 }
